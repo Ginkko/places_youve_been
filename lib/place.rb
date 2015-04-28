f06dc6dd79a53a7f05ffcb0385383e570e0f4d82
Alec Arme & Bob Ellis <student@epicodus.com>
2015-04-28 12:04:28 -0700

    add spec .save and code to pass

--- lib/place.rb
+++ lib/place.rb
@@ -1,10 +1,17 @@
 class Place
+
+@@places = []
+
   define_method(:initialize) do |name|
     @name = name
   end
 
     define_singleton_method(:all) do
-      []
+      @@places
+    end
+
+    define_method(:save) do
+      @@places.push(self)
     end
 
 end
--- spec/place_spec.rb
+++ spec/place_spec.rb
@@ -3,10 +3,21 @@ require('place')
 require('pry')
 
 describe(Place) do
+
   describe('.all') do
     it('returns all elements of place') do
       expect(Place.all()).to(eq([]))
     end
+  end
+
+  describe('#save') do
+    it('adds a place to the list of places') do
+      test_place = Place.new('Portland')
+      test_place.save()
+      #expect(Place.all()).to(eq(['Portland']))
+      expect(Place.all()).to(eq([test_place]))
+    end
+
   end
 
 end
